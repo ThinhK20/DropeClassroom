5fd994e55c9b72a0e96977f75079da2b736308ea
huyhoanfithcmus <huyhoan652002@gmail.com>
2023-11-21 02:10:21 +0700

    add UI mark table

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import TeacherMarkPage from "../components/markPage/teacher";
+import StudentMarkPage from "../components/markPage/student";
 
 export const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <App />,
-   },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/user/teacher/course/mark",
+    element: <TeacherMarkPage />,
+  },
+  {
+    path: "/user/student/course/mark",
+    element: <StudentMarkPage />,
+  },
 ]);
