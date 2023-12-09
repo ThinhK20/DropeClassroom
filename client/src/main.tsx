43ebb7982c35aa8aec2acaf3bf17dc264f54e61e
ThinhK20 <74862129+ThinhK20@users.noreply.github.com>
2023-12-10 01:08:16 +0700

    Update redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,20 @@ import "./shared/css/index.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <Provider store={store}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-         <RouterProvider router={router} />
-      </LocalizationProvider>
+      <PersistGate persistor={persistor}>
+         <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <RouterProvider router={router} />
+            <ToastContainer />
+         </LocalizationProvider>
+      </PersistGate>
    </Provider>
 );
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,19 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createClassSlice from "./createClassSlice";
 import joinClassSlice from "./joinClassSlice";
 import userClassroomSlice from "./userClassroomSlice";
 import userSlice from "./userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const rootReducer = combineReducers({
+   users: userSlice,
+   createClass: createClassSlice,
+   joinClass: joinClassSlice,
+   userClassroom: userClassroomSlice,
+});
 
+const persistConfig = {
+   key: "root",
+   storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-   reducer: {
-      users: userSlice,
-      createClass: createClassSlice,
-      joinClass: joinClassSlice,
-      userClassroom: userClassroomSlice  
-   },
+   reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
