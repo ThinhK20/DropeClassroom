174523e5a1d32abff833aad3e781f5dc62f17df3
hhman24 <anhoang483@gmail.com>
2024-01-07 00:33:10 +0700

    hhman

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,9 +14,9 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
-        secure: true,
-        sameSite: 'lax',
-        httpOnly: false,
+        // secure: true,
+        // sameSite: 'lax',
+        // httpOnly: false,
       },
     }),
   );
