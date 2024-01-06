3f7fef94d26e107d22990eaadc922eecc44fe143
huyhoanFithcmus <huyhoan652002@gmail.com>
2024-01-07 00:38:45 +0700

    u -> U

--- client/src/components/modal/UpdateAssignmentModal.tsx
+++ client/src/components/modal/UpdateAssignmentModal.tsx
@@ -133,7 +133,7 @@ export default function UpdateAssignmentModal(props: {
             <div className="space-y-12 w-full">
               <div className="border-b border-gray-900/10 pb-12">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
-                  update Assignment
+                  Update Assignment
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
                   Update assignment at {currentClass?.classId.className}
